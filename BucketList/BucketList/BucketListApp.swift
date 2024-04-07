f0a1f995154f2486aab9401770ca6b850e481d77
Vanya <whitewolf3310@icloud.com>
2024-04-07 13:18:35 +0300

    add new project

---
+++ BucketList/BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Ваня Науменко on 7.04.24.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BucketList/BucketList/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BucketList
+//
+//  Created by Ваня Науменко on 7.04.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
